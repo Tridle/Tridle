510845ac087b02a502dcc7afca16c7a5ee36dc83
Duan Yi <yumendy@163.com>
2014-11-29 11:20:41 +0800

    models & admin finish

--- Tridle/TridleWeb/admin.py
+++ Tridle/TridleWeb/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from TridleWeb.models import *
 
 # Register your models here.
+
+class MyUserAdmin(admin.ModelAdmin):
+        '''
+            Admin View for MyUser
+        '''
+        list_display = ('nickname',)
+        list_filter = ('usertype','permission')
+        search_fields = ['nickname']
+
+class LetterAdmin(admin.ModelAdmin):
+        '''
+            Admin View for Letter
+        '''
+        list_display = ('title','time','flag','user_from','user_to')
+        list_filter = ('time','flag')
+        search_fields = ['title','context']
+
+class ProductModelAdmin(admin.ModelAdmin):
+        '''
+            Admin View for ProductModel
+        '''
+        list_display = ('name','date','price','published','author')
+        list_filter = ('author','published')
+        search_fields = ['name','author']
+
+class CommentAdmin(admin.ModelAdmin):
+        '''
+            Admin View for Comment
+        '''
+        list_display = ('author','date','produce','starts')
+        list_filter = ('author','produce','starts')
+        search_fields = ['author','produce','context']
+
+class PicAdmin(admin.ModelAdmin):
+        '''
+            Admin View for Pic
+        '''
+        list_display = ('produce',)
+        list_filter = ('produce',)
+        search_fields = ['produce']
+
+class ShowAdmin(admin.ModelAdmin):
+        '''
+            Admin View for Show
+        '''
+        list_display = ('title',)
+        search_fields = ['descript','title']
+
+admin.register(Show, ShowAdmin)
+
+admin.register(Pic, PicAdmin)
+
+admin.register(Comment, CommentAdmin)
+
+admin.register(ProductModel, ProductModelAdmin)
+
+admin.register(Letter, LetterAdmin)
+
+admin.register(MyUser, MyUserAdmin)
--- Tridle/TridleWeb/models.py
+++ Tridle/TridleWeb/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MyUser(models.Model):
+	permission = models.IntegerField()
+	nickname = models.CharField(max_length=64)
+	user_type = models.CharField(max_length=128)
+	headimg = models.ImageField(upload_to='./image/')
+
+	user = models.OneToOneField(User)
+
+	class Meta:
+		ording = ('nickname',)
+	
+	def __unicode__(self):
+		return self.nickname
+
+class Letter(models.Model):
+	title = models.CharField(max_length = 64)
+	context = models.TextField()
+	time = models.DateTimeField(auto_now=True)
+	flag = models.BooleanField(default=False)
+
+	user_from = models.ForeignKey(MyUser)
+	user_to = models.ForeignKey(MyUser)
+
+	def __unicode__(self):
+		return self.title
+
+class ProductModel(models.Model):
+	name = models.CharField(max_length=64)
+	date = models.DateTimeField(auto_now=True)
+	price = models.FloatField()
+	pic_3D = models.URLField()
+	published = models.BooleanField(default=False)
+
+	author = models.ForeignKey(MyUser)
+	favour = models.ManyToManyField(MyUser)
+
+	def __unicode__(self):
+		return self.name
+
+class Comment(models.Model):
+	context = models.TextField()
+	date = models.DateTimeField(auto_now=True)
+	starts = models.IntegerField()
+
+	author = models.ForeignKey(MyUser)
+	produce = models.ForeignKey(ProductModel)
+
+	def __unicode__(self):
+		return self.context
+
+class Pic(models.Model):
+	descript = models.TextField()
+	img = models.ImageField()
+
+	produce = models.ForeignKey(ProductModel)
+
+	def __unicode__(self):
+		return self.descript
+
+class Show(models.Model):
+	title = models.CharField(max_length=64)
+	descript = models.TextField()
+	number = models.IntegerField()
+	link = models.URLField()
+
+	picture = models.ForeignKey(Pic)
+
+	def __unicode__(self):
+		return self.descript
